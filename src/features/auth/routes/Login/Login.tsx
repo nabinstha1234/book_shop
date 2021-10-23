d64e99b48d5868565cc75aa44c2482940c8e9223
Nabin Shrestha <snabin@insightworkshop.io>
2021-10-23 18:04:25 +0545

    feat: add compoent structure for auth

---
+++ src/features/auth/routes/Login/Login.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import styles from './Login.module.scss';
+interface Props {}
+
+export const Login = (props: Props) => {
+  return <div className={styles['container']}>I am login page</div>;
+};
