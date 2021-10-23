e1c8b10e1d922955a64e7c6341debfcc51cef434
Nabin Shrestha <snabin@insightworkshop.io>
2021-10-23 22:53:43 +0545

    feat: add header skeleton

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Icon } from '@iconify/react';
 
 import styles from './Header.module.scss';
 
@@ -11,27 +12,29 @@ const Header = ({ isLoggedIn = false }: Props) => {
   return (
     <nav className={styles['navbar']}>
       <div className={styles['container']}>
-        <div className="navbar-header">
+        <div className={styles['navbar-header']}>
           <Link to="/">
             <h4>
-              Awesome<span>logo</span>
+              Awesome <span>logo</span>
             </h4>
           </Link>
         </div>
-
-        <div className="navbar-menu" id="open-navbar1">
-          <ul className="navbar-nav">
-            <li className="active">
-              <Link to="/">Home</Link>
+        <div className={styles['search']}>
+          <input type="text" />
+          <Icon className={styles['search-icon']} icon="mdi:magnify" />
+        </div>
+        <div className={styles['navbar-menu']} id="open-navbar1">
+          <ul className={styles['navbar-nav']}>
+            <li className={styles['nav-item']}>
+              <span className={styles['nav-text']}>Rs.</span> 4567.78 &nbsp;
+              <div className={styles['cart-wrapper']}>
+                <Icon className="icon-cart" icon="mdi:cart" />
+              </div>
+              <div className={styles['count']}>10</div>
             </li>
-            <li>
-              <Link to="/">About</Link>
-            </li>
-            <li>
-              <Link to="/">Contact</Link>
-            </li>
-            <li>
-              <Link to="/">Signin</Link>
+            <li className={styles['nav-item']}>
+              <span className={styles['nav-text']}>Hello,</span> Nabin &nbsp;
+              <Icon className={styles['user-icon']} icon="mdi:account-circle" />
             </li>
           </ul>
         </div>
