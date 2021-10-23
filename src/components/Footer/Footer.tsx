22dfea3bc5e7631136cc4456f5a0779a1e9145e8
Nabin Shrestha <snabin@insightworkshop.io>
2021-10-23 18:03:02 +0545

    feat: add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {}
+
+const Footer = (props: Props) => {
+  return <div className="text-center py-2">Develop By Nabin</div>;
+};
+
+export default Footer;
